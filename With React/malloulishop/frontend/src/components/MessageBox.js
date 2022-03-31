8dfb626b7c4f3d8fe8e9831f0c73ec84122bc07f
nasser-mallouli <nassermallouli@gmail.com>
2022-03-31 14:17:19 +0200

    Update MessageBox with 404 Image

--- With React/malloulishop/frontend/src/components/MessageBox.js
+++ With React/malloulishop/frontend/src/components/MessageBox.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function MessageBox(props) {
   return (
-    <div className={`alert alert-${props.variant || 'info'}`}>{props.children}</div>
+    <div className={`row alert alert-${props.variant || 'info'}`}>
+      <ul>
+        <li>
+      <img src='./images/PageNotFound.png' alt='Page Not Found'/>
+        </li>
+        <li>
+      <p>{props.children}</p>
+        </li>
+      </ul>
+      </div>
   )
 }
